fa24a35d9d0993e1af875c49bcdccd18e34af9bc
derickcastillo38 <9derick@gmail.com>
2018-10-01 19:57:05 -0400

    added ability to create a new profile card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import DrinkContainer from './DrinkContainer'
-import FaceContainer from './FaceContainer'
+import Drinkcards from './Drinkcards'
 import DrinkForm from './DrinkForm'
 
 class App extends React.Component {
@@ -9,32 +8,108 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      margaritas: [],
-      faces: []
+      drinkcards: [],
+      nameInput: "",
+      descriptionInput: "",
+      drinkInput: "",
+      moodInput: ""
     }
   }
 
   componentDidMount() {
-    fetch('http://localhost:3000/beverages')
+    fetch('http://localhost:3000/drinkcards')
     .then(res => res.json())
-    .then(margaritas =>
-      this.setState({margaritas: margaritas})
-    )
-
-    fetch('http://localhost:3000/faces')
-    .then(res => res.json())
-    .then(faces =>
-      this.setState({faces: faces})
+    .then(drinkcards =>
+      this.setState({drinkcards: drinkcards})
     )
   }
+
+  margaritaImage = (drink) => {
+    if (drink === 'Margarita') {
+      return "https://www.thecocktaildb.com/images/media/drink/wpxpvu1439905379.jpg"
+    } else if (drink === "Blue Margarita") {
+      return "https://www.thecocktaildb.com/images/media/drink/qtvvyq1439905913.jpg"
+    } else if (drink === "Tommy's Margarita") {
+      return "https://www.thecocktaildb.com/images/media/drink/loezxn1504373874.jpg"
+    } else if (drink === "Whitecap Margarita") {
+      return "https://www.thecocktaildb.com/images/media/drink/srpxxp1441209622.jpg"
+    } else if (drink === "Strawberry Margarita") {
+      return "https://www.thecocktaildb.com/images/media/drink/tqyrpw1439905311.jpg"
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    fetch('http://localhost:3000/drinkcards', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        user: this.state.nameInput,
+        beverage: this.state.drinkInput,
+        mood: this.state.moodInput,
+        description: this.state.descriptionInput,
+        image: this.margaritaImage(this.state.drinkInput)
+      })
+    })
+    .then(response => response.json())
+    .then(drinkcard => {
+      this.setState({
+        drinkcards: [...this.state.drinkcards, drinkcard].reverse()
+      })
+    })
+    this.state.nameInput = ""
+    this.state.descriptionInput = ""
+  }
+
+  chooseCards = () => {
+    return this.state.drinkcards
+  }
+
+  chooseName = () => {
+    return this.state.nameInput
+  }
+
+  chooseMood = () => {
+    return this.state.moodInput
+  }
+
+  chooseDrink = () => {
+    return this.state.drinkInput
+  }
+
+  handleNameChange = (e) => {
+    this.setState({nameInput: e.target.value})
+  }
+
+  handleDescriptionChange = (e) => {
+    this.setState({descriptionInput: e.target.value})
+  }
+
+  handleMoodChange = (e) => {
+    this.setState({moodInput: e.target.value})
+  }
+
+  handleDrinkChange = (e) => {
+    this.setState({drinkInput: e.target.value})
+  }
+
+
 
   render() {
     return (
       <div className="App">
         <div className="Body">
-          <DrinkContainer drinks={this.state.margaritas}/>
-          <DrinkForm/>
-          <FaceContainer faces={this.state.faces}/>
+          <DrinkForm
+            drinkInput={this.chooseDrink()}
+            moodInput={this.chooseMood()}
+            nameInput={this.chooseName()}
+            descriptionInput={this.state.descriptionInput}
+            handleNameChange={this.handleNameChange}
+            handleDescriptionChange={this.handleDescriptionChange}
+            handleMoodChange={this.handleMoodChange}
+            handleDrinkChange={this.handleDrinkChange}
+            handleSubmit={this.handleSubmit}/>
+          <Drinkcards cards={this.chooseCards()}/>
         </div>
       </div>
     )
--- src/DrinkForm.js
+++ src/DrinkForm.js
@@ -2,23 +2,43 @@ import React from 'react'
 
 class DrinkForm extends React.Component {
   render() {
+    console.log(this.props);
     return (
-      <div class="form-div">
+      <div className="form-div">
+      <br></br>
+      <br></br>
+      <hr></hr>
+      <hr></hr>
+      <br></br>
       <h2 id='profile'>Create Flat-Lounge Profile</h2>
       <br></br>
       <form onSubmit={this.props.handleSubmit} className='drink-form'>
-      <input type="text" name="name" placeholder="Enter name here:"></input>
+      <input
+        type="text"
+        value={this.props.nameInput}
+        name="nameInput"
+        onChange={this.props.handleNameChange}
+        placeholder="Enter name here:">
+      </input>
       <br></br>
       <br></br>
       <h6>Choose Drink & Emotion</h6>
-        <select name="drink">
+        <select
+        onChange={this.props.handleDrinkChange}
+        value={this.props.value}
+        name="drink"
+        id='drink-dropdown'>
           <option value="Margarita">Margarita</option>
           <option value="Blue Margarita">Blue Margarita</option>
           <option value="Tommy's Margarita">Tommys Margarita</option>
           <option value="Whitecap Margarita">Whitecap Margarita</option>
           <option value="Strawberry Margarita">Strawberry Margarita</option>
         </select>
-        <select name="emotion">
+        <select
+          name="emotion"
+          id='emotion-dropdown'
+          onChange={this.props.handleMoodChange}
+          value={this.props.value}>
           <option value="Ecstatic">Ecstatic</option>
           <option value="So-so">So-so</option>
           <option value="Frustrated">Frustrated</option>
@@ -27,9 +47,19 @@ class DrinkForm extends React.Component {
         </select>
         <br></br>
         <br></br>
-        <input type="text" name="description" placeholder="Describe your emotion:"></input>
+        <input
+          type="text"
+          name="descriptionInput"
+          value={this.props.descriptionInput}
+          onChange={this.props.handleDescriptionChange}
+          placeholder="Describe your emotion:">
+        </input>
         <br></br>
-        <input type="submit" value="Submit"></input>
+        <button type="submit">Submit</button>
+        <br></br>
+        <br></br>
+        <hr></hr>
+        <hr></hr>
       </form>
       </div>
     )
